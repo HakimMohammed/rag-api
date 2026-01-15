70593628b2eb3beb3995cf5c17568c8196a2e0aa
HakimMohammed <mohamed.hakkim.dev@gmail.com>
2026-01-15 21:31:13 +0100

    Embedding Creation

---
+++ embed.py
@@ -0,0 +1,16 @@
+'''
+This file is responsible for converting our txt file into vectors to be stored in ChromaDB.
+Those vectors will be later used as context by our local LLM.
+'''
+
+import chromadb
+
+client = chromadb.PersistentClient(path="./db")
+collection = client.get_or_create_collection("docs")
+
+with open("k8s.txt", "r") as file:
+    text = file.read()
+
+collection.add(documents=[text], ids=["k8s"])
+
+print("Embedding stored in chroma")
