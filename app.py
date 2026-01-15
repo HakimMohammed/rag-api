d35655cd569cae6c11497234fa37391c983a5ccd
HakimMohammed <mohamed.hakkim.dev@gmail.com>
2026-01-15 21:53:57 +0100

    RAG API

---
+++ app.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import chromadb
+import ollama
+
+app = FastAPI()
+client = chromadb.PersistentClient(path="./db")
+collection = client.get_collection("docs")
+
+@app.get("/query")
+def queryChroma(query: str):
+    results = collection.query(query_texts=[query], n_results=1)
+    context = results["documents"][0][0] if results["documents"] else ""
+    
+    answer = ollama.generate(
+        model="tinyllama",
+        prompt=f"""
+        Context:
+        {context}
+        
+        
+        Question:
+        {query}
+        
+        Answer clearly and precisely:
+        """
+    )
+    
+    return {"answer": answer["response"]}
